150297afcafc4ba47340a5c0eb2e7f9fcdb6599b
Zach Ruiz <powersbooth10@gmail.com>
2022-10-16 23:58:48 -0500

    commit tree e1abc98b7704c00276409eaa9630d3d81de0074e

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,38 @@
 # your code goes here
+class Person
+    attr_reader :name
+    attr_accessor :bank_account, :happiness, :hygiene
+
+    def initialize(name)
+        @name = name
+        @bank_account = 25
+        @happiness = 8
+        @hygiene = 8
+    end
+    def happy?
+        self.happiness > 7
+    end
+    def clean?
+        self.hygiene > 7
+    end
+    def get_paid(amount)
+        self.bank_account += amount
+        'all about the benjamins'
+    end
+    def take_bath
+        self.hygiene += 4
+        '♪ Rub-a-dub just relaxing in the tub ♫'
+    end
+    def work_out
+        self.hygiene -= 3
+        self.happiness += 2
+        '♪ another one bites the dust ♫'
+    end
+    def call_friend()
+        self.happiness += 3
+
+    end
+    def start_conversation
+
+    end
+end
